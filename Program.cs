0555a0d54cd4e65418f27f5fbb863a9a7f8f3982
Ody Mbegbu <odytrice@gmail.com>
2018-03-10 12:32:58 +0100

    Week Two: While and For Loops

--- Program.cs
+++ Program.cs
@@ -6,143 +6,21 @@ namespace Vatebra.Testing
     {
         static void Main(string[] args)
         {
-            //Get the Number
-            Console.Write("Enter the First Number: ");
-            string inputText = Console.ReadLine();
-            int number = int.Parse(inputText);
-
-
-            /*
-             * Solution 1: if the number is 0..10 then it should say if it is odd or even.
-             * Any other thing should say invalid
-             */
-
-            switch (number)
+            int x = 0;
+            while (x <= 10)
             {
-                case 0: Console.WriteLine("Even"); break;
-                case 1: Console.WriteLine("Odd"); break;
-                case 2: Console.WriteLine("Even"); break;
-                case 3: Console.WriteLine("Odd"); break;
-                case 4: Console.WriteLine("Even");break;
-                case 5: Console.WriteLine("Odd"); break;
-                case 6: Console.WriteLine("Even"); break;
-                case 7: Console.WriteLine("Odd"); break;
-                case 8: Console.WriteLine("Even"); break;
-                case 9: Console.WriteLine("Odd"); break;
-                case 10: Console.WriteLine("Even"); break;
-                default: Console.WriteLine("Invalid Number"); break;
+                Console.WriteLine(x);
+                x = x + 1;
             }
 
-            switch (number)
+            for (int i = 0; i <= 10; i++)
             {
-                case 0: 
-                case 2: 
-                case 4: 
-                case 6: 
-                case 8: 
-                case 10: Console.WriteLine("Even"); break;
-                case 1: 
-                case 3: 
-                case 5: 
-                case 7: 
-                case 9: Console.WriteLine("Odd"); break;
-                
-                default: Console.WriteLine("Invalid Number"); break;
+                Console.WriteLine(i);
             }
 
-            /*
-             * Solution 2: if the number is < 0 or > 10 then it should say invalid.
-             * Any other thing should say if it is odd or even
-             */
-
-            if(number < 0)
+            for (int j = 0; j < 10; j++)
             {
-                Console.WriteLine("Invalid Number");
-            }
-            else if(number > 10)
-            {
-                Console.WriteLine("Invalid Number");
-            }
-            else
-            {
-                //This means that number is between 0 and 10
-                switch (number)
-                {
-                    case 0:
-                    case 2:
-                    case 4:
-                    case 6:
-                    case 8:
-                    case 10: Console.WriteLine("Even"); break;
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 9: Console.WriteLine("Odd"); break;
-                }
-            }
-
-            /*
-             * Solution 3: if the number is < 0 or > 10 then it should say invalid.
-             * if the remainder of dividing the number by 2 is 0 then it should say even
-             * otherwise it should say odd
-             */
-
-            if (number < 0)
-            {
-                Console.WriteLine("Invalid Number");
-            }
-            else if (number > 10)
-            {
-                Console.WriteLine("Invalid Number");
-            }
-            else
-            {
-                //This means that number is between 0 and 10
-                if(number % 2 == 0)
-                {
-                    Console.WriteLine("Even");
-                }
-                else
-                {
-                    Console.WriteLine("Odd");
-                }
-            }
-
-            //Using Combinations
-            if (number < 0 || number > 10)
-            {
-                Console.WriteLine("Invalid Number");
-            }
-            else
-            {
-                //This means that number is between 0 and 10
-                if (number % 2 == 0)
-                {
-                    Console.WriteLine("Even");
-                }
-                else
-                {
-                    Console.WriteLine("Odd");
-                }
-            }
-
-
-            if (number >= 0 && number <= 10)
-            {
-                //This means that number is between 0 and 10
-                if (number % 2 == 0)
-                {
-                    Console.WriteLine("Even");
-                }
-                else
-                {
-                    Console.WriteLine("Odd");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid Number");
+                Console.WriteLine(j);
             }
         }
     }
